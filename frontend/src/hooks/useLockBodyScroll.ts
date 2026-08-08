98cebf4344c7f621af47bb383758c48ed2c0da89
samuelcarvalh00 <seuemail@example.com>
2026-08-08 20:36:28 -0300

    ajustando o design do projeto

--- frontend/src/hooks/useLockBodyScroll.ts
+++ frontend/src/hooks/useLockBodyScroll.ts
@@ -1,27 +1,11 @@
 import { useEffect } from "react";
 
-let scrollY = 0;
-
 export function useLockBodyScroll(ativo: boolean) {
   useEffect(() => {
-    if (ativo) {
-      scrollY = window.scrollY;
-      document.body.style.position = "fixed";
-      document.body.style.top = `-${scrollY}px`;
-      document.body.style.left = "0";
-      document.body.style.right = "0";
-      document.body.style.width = "100%";
-      document.body.style.overflow = "hidden";
-    }
-
+    if (!ativo) return;
+    document.body.style.overflow = "hidden";
     return () => {
-      document.body.style.position = "";
-      document.body.style.top = "";
-      document.body.style.left = "";
-      document.body.style.right = "";
-      document.body.style.width = "";
       document.body.style.overflow = "";
-      window.scrollTo(0, scrollY);
     };
   }, [ativo]);
 }
